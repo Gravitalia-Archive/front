47ea42cb7e7f390c596ac7acaf7f623d55985b87
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-01-18 20:19:26 +0100

    Working i18n 🌐

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
                 cookieKey: "lang",
                 redirectOn: "no_prefix",
                 fallbackLocale: "en",
-                alwaysRedirect: false
+                alwaysRedirect: true
             },
             locales: [
                 {
