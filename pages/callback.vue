737301abbd1e9ce6b08761713506fff602b5a70c
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-05-01 17:24:59 +0200

    Adding callback route

---
+++ pages/callback.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script setup>
+    const route = useRoute();
+    if(!route.query.token) {
+        await navigateTo("https://api.gravitalia.com/callback", { external: true, redirectCode: 307 });
+    }
+
+    const token = useCookie("token", { maxAge: 604800, httpOnly: false, secure: true, sameSite: "strict" });
+    token.value = route.query.token;
+    await navigateTo("/");
+</script>
