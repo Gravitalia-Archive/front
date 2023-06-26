71937695f8eaabaae1215f12a1a023c82d45a19a
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-06-26 19:49:39 +0200

    Delete index.js

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-const idList = [ "take", "publish", "share", "like" ];
-let last = "take";
-setInterval(() => {
-    document.getElementById(last).classList.add("hidden");
-    last = idList[idList.indexOf(last)+1];
-    if(!last) last = "take";
-    document.getElementById(last).classList.remove("hidden");
-}, 2580)
