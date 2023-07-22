c8079672253bdbd4d6d709a99d6a9d4cb4f1b4ee
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-23 01:26:23 +0200

    Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -85,46 +85,6 @@ export default defineNuxtConfig({
             SITE_URL: "https://www.gravitalia.com"
         }
     },
-
-    hooks: {
-        "build:before": async () => {
-            const myHeaders = new Headers();
-            myHeaders.append("Authorization", "CODE")
-
-            let xml: string = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd
-    http://www.w3.org/1999/xhtml http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd"
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xhtml="http://www.w3.org/1999/xhtml"   
->
-<url>
-    <loc>https://www.gravitalia.com/</loc>
-    <xhtml:link rel="alternate" hreflang="en" href="https://www.gravitalia.com/" />
-    <xhtml:link rel="alternate" hreflang="fr" href="https://www.gravitalia.com/" />
-</url>
-<url>
-    <loc>https://www.gravitalia.com/explore</loc>
-</url>`;
-
-            const posts: any = await fetch("https://api.gravitalia.com/search/all_users", {
-                headers: myHeaders
-            }).then(res => res.json());
-
-            posts.map((p: string) => {
-                xml += `
-<url>
-    <loc>https://www.gravitalia.com/${p}</loc>
-    <priority>0.8</priority>
-</url>`;
-            });
-
-            xml += "</urlset>";
-
-            fs.writeFileSync("./public/sitemap.xml", xml);
-        }
-    },
   
     nitro: {
         preset: 'cloudflare'
